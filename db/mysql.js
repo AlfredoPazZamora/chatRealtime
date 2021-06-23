b093a4bfbe1c3d06971d1f580606fb8d5abfe072
AlfredoPazZamora <46512729+AlfredoPazZamora@users.noreply.github.com>
2021-06-23 12:28:11 -0500

    Deploy en heroku DB

--- db/mysql.js
+++ db/mysql.js
@@ -2,10 +2,10 @@ let mysql = require('mysql');
 let config = require('../config');
 
 let connection = mysql.createConnection({
-    host: config.HOST,
-    user: config.USER,
-    password: config.PW,
-    database: config.DB
+    host: 'us-cdbr-east-04.cleardb.com',
+    user: 'be6e7059cd1c8a',
+    password: '94db54d6',
+    database: 'heroku_08b9a5b304298d8'
 });
 
 connection.connect(
